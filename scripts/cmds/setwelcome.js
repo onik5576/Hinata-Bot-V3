0b2db8c0901894c6bda6a102b5d613a2f658e3e2
onik5576 <mayaakther3377@gmail.com>
2026-09-23 10:09:27 +0600

    Update welcome message to include Bengali text

--- scripts/cmds/setwelcome.js
+++ scripts/cmds/setwelcome.js
@@ -39,7 +39,7 @@ module.exports = {
 					+ "\n  + {multiple}: you || you guys"
 					+ "\n  + {session}:  session in day"
 					+ "\n\n   Example:"
-					+ "\n    {pn} text Hello {userName}, welcome to {boxName}, have a nice day {multiple}"
+					+ "\n    {pn} text আসসালামু ওয়ালাইকুম {userName}, welcome to {boxName}, পাখি আপনাকে অনিক এর পক্ষ থেকে মন বড়া ভালোবাসা 😩😁{multiple}"
 					+ "\n"
 					+ "\n   Reply (phản hồi) or send a message with file with content {pn} file: to add file attachments to welcome message (image, video, audio)"
 					+ "\n\n   Example:"
